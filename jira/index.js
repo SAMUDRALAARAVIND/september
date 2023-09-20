f83a5b8de2ef926d45a0e7be9ca96242a07422a9
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-09-20 23:11:30 +0530

    Jira clone

---
+++ jira/index.js
@@ -0,0 +1,53 @@
+// Handling button click
+const createButtons = document.querySelectorAll("section > div > button");
+function createTask(event) {
+  // we need know which button is clicked !!!
+  //   event.target => <button>Create Issue</button>
+  const textInput = event.target.nextElementSibling; //
+  textInput.className = "show";
+}
+
+// Adding event listeners for create buttons
+for (let i = 0; i < createButtons.length; i++) {
+  // createButtons[i]  is a <button></button>
+  createButtons[i].addEventListener("click", createTask);
+}
+
+// Handling Input
+const inputElements = document.querySelectorAll("section > div > input");
+
+function deleteTask(deleteButton) {
+  // this one will be executed when user clicks on the delete button of a task card.
+  const parentCard = deleteButton.parentNode;
+  parentCard.remove();
+}
+
+// this function will listen to keyup event for the input elements
+// write the logic to create the card in the corresponding section.
+function handleInput(event) {
+  // event.keyCode = 13 represents that user has clicked on Enter key
+  const inputElement = event.target; // <input />
+  if (event.keyCode === 13) {
+    let taskName = inputElement.value;
+
+    const card = document.createElement("div");
+    card.className = "card";
+    card.draggable = "true";
+    card.innerHTML = `
+        <b>${taskName}</b>
+        <button onclick="deleteTask(this)">delete</button>
+    `;
+
+    // the above card should go inside the <div class="cards"></div>
+    const cardsContainer = inputElement.nextElementSibling;
+    cardsContainer.appendChild(card);
+
+    // emptying the input
+    inputElement.value = "";
+    inputElement.className = "hide";
+  }
+}
+
+for (let i = 0; i < inputElements.length; i++) {
+  inputElements[i].addEventListener("keyup", handleInput);
+}
---
+++ jira/test.js
@@ -0,0 +1,33 @@
+const dragElement = document.getElementById("drag");
+const dropElement = document.getElementById("drop");
+
+// when user starts dragging the element.
+// dragElement.addEventListener("dragstart", () => {
+//   console.log("started dragging");
+// });
+
+// when user stops dragging
+// dragElement.addEventListener("dragend", () => {
+//   console.log("stopped dragging");
+// });
+
+// when any dragged element enters into `dropElement` then the below event will be triggered
+// dropElement.addEventListener("dragenter", () => {
+//   console.log("drag enter event");
+// });
+
+// when a tragged element is active on the `dropElement` then the below will be triggered.
+// let i = 0;
+// dropElement.addEventListener("dragover", (event) => {
+//   // it will ensure to trigger the drop event
+//   event.preventDefault();
+//   //   console.log("drag over", ++i);
+// });
+
+// dropElement.addEventListener("dragleave", () => {
+//   console.log("drag leave");
+// });
+
+// dropElement.addEventListener("drop", () => {
+//   console.log("drop event");
+// });
