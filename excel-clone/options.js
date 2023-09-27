f195eaedd33f41d7c42d1861dc5dbfe6a296b4f2
SAMUDRALA ARAVIND <67514829+SAMUDRALAARAVIND@users.noreply.github.com>
2023-09-27 23:20:56 +0530

    Excel sheet class 3 notes

--- excel-clone/options.js
+++ excel-clone/options.js
@@ -2,6 +2,9 @@
 
 const activeCellElement = document.getElementById("active-cell");
 const textAlignElements = document.getElementsByClassName("text-align");
+const boldButton = document.getElementById("bold");
+const italicButton = document.getElementById("italic");
+const underlinedButton = document.getElementById("underlined");
 
 // activeCell defines which cell is selected / active.
 // intially it will null indicating that no cell is selected.
@@ -9,24 +12,73 @@ let activeCell = null;
 
 let activeOptionsState;
 
+function toggleButtonsStyle(button, isSelected) {
+  if (isSelected) {
+    // currently selected cell in the bold state.
+    button.classList.add("active-option");
+  } else {
+    button.classList.remove("active-option");
+  }
+}
+
+function highlightOptionButtonsOnFocus() {
+  // check if the cell is in the bold state or not .
+  // if (activeOptionsState.isBoldSelected) {
+  //   // currently selected cell in the bold state.
+  //   boldButton.classList.add("active-option");
+  // } else {
+  //   boldButton.classList.remove("active-option");
+  // }
+
+  toggleButtonsStyle(boldButton, activeOptionsState.isBoldSelected);
+
+  // check if the selected cell is italic or not .
+  // if (activeOptionsState.isItalicSelected) {
+  //   // the current cell is italic text.
+  //   italicButton.classList.add("active-option");
+  // } else {
+  //   italicButton.classList.remove("active-option");
+  // }
+  toggleButtonsStyle(italicButton, activeOptionsState.isItalicSelected);
+
+  // check if the selected cell is underline or not .
+
+  // if (activeOptionsState.isUnderLineSelected) {
+  //   // the cell is underlined
+  //   underlinedButton.classList.add("active-option");
+  // } else {
+  //   underlinedButton.classList.remove("active-option");
+  // }
+  toggleButtonsStyle(underlinedButton, activeOptionsState.isUnderLineSelected);
+
+  // get the textAlign value
+  highlightTextAlignButtons(activeOptionsState.textAlign);
+  // highlightTextAlignButtons("start" | "right" | "center")
+}
+
 // below function will be triggered whenever cell is focused.
 function onCellFocus(e) {
   // whenever a cell is focused change the activeCell value to be the id of cell.
+  if (activeCell && activeCell.id === e.target.id) {
+    // previously selected cell is equal to the currently selected cell.
+    return;
+  }
   activeCell = e.target;
   activeCellElement.innerText = e.target.id;
   // intialise the state of this cell.
   const computedStyle = getComputedStyle(activeCell);
-
   activeOptionsState = {
     fontFamily: computedStyle.fontFamily,
     isBoldSelected: computedStyle.fontWeight === "600",
     isItalicSelected: computedStyle.fontStyle === "italic",
-    isUnderLineSelected: computedStyle.textDecoration === "underline",
+    isUnderLineSelected: computedStyle.textDecoration.includes("underline"),
     textAlign: computedStyle.textAlign,
     textColor: computedStyle.color,
     backgroundColor: computedStyle.backgroundColor,
     fontSize: computedStyle.fontSize,
   };
+
+  highlightOptionButtonsOnFocus();
 }
 
 function onClickBold(boldButton) {
@@ -78,6 +130,7 @@ function onClickUnderline(underlinedButton) {
   }
 }
 
+// the below function task is to take the textAlign value and decides which alignment button needs to highlighted or not.
 function highlightTextAlignButtons(textAlignValue) {
   // textAlignValue === "left" => we have to highlight only left button
   // textAlignValue === "right" => we have to highlight only right button
